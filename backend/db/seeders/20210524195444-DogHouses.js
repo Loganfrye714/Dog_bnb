61bb5701a5811c7524a91f1c2c9fc3b6f48973dc
Logan.Frye <Loganfrye714@gmail.com>
2021-05-24 13:16:43 -0700

    seed data

--- backend/db/seeders/20210524195444-DogHouses.js
+++ backend/db/seeders/20210524195444-DogHouses.js
@@ -2,25 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    return queryInterface.bulkInsert('DogHouses', [
+        { totalDogsAllowed: 2, totalDogMats: 3, totalYardBathrooms: 1, summary: 'Beautiful two bedroom dog house with all of the amenties you could possibly want!', location: "Seattle, Washington", toys: true, dogFood: true, dogWalker: true, treats: true, price: 349.00, imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/711QAb2XypL._AC_SL1500_.jpg', createdAt: new Date(), updatedAt: new Date()},
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+        { totalDogsAllowed: 1, totalDogMats: 1, totalYardBathrooms: 1, summary: 'Modern home for those dogs who love design....', location: "Lakeview, Michigan", toys: true, dogFood: true, dogWalker: false, treats: true, price: 349.00, imgUrl: 'https://media.istockphoto.com/photos/funny-pug-dog-in-the-dog-house-picture-id516461357?k=6&m=516461357&s=612x612&w=0&h=C7L1EWt-JCUcTzHWG4C-jH3jzsFERZam2fxB2cdj0LA=', createdAt: new Date(), updatedAt: new Date()},
+      ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('DogHouses', null, {});
   }
 };
